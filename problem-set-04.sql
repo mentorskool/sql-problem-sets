9a020b1db423c1c87f4889e4ed03faf2d29ec12d
Mentorskool <learn@mentorskool.com>
2022-05-17 12:31:43 +0530

    Data processing and wrangling

---
+++ problem-set-04.sql
@@ -0,0 +1,70 @@
+-- Tables to be referred to : 
+-- customers
+-- orders
+-- products
+-- transactions
+-- vendors
+-- returns
+-- location_01
+
+-- Q1) How many sales transactions took place in Jun'17?
+-- 
+
+
+-- Q2) Show State wise profit. Output should be sorted by profit in decreasing order
+
+
+
+
+
+-- Q3) Show State wise profit. Output should be sorted by profit in decreasing order. Show top 5 states as the final output
+
+
+
+
+-- Q4) Show State wise profit margin. Profit Margin should be shown as percentage
+-- and should be rounded off to 2 decimal places. Show top 5 states as the final output
+
+
+
+-- Q5) Show sales by year and month and then 
+-- filter the result for last 3 months of 2017
+-- Output format : 
+-----------------------
+-- Month-Year | Sales 
+-----------------------
+-- Jan-2017 | 1000.25
+
+
+-- Q6) Calculate sales contribution% for each of the category by state
+-- Output format : 
+----------------------------------------
+-- Category         | Sales_Contribution
+----------------------------------------
+-- Technology       | 70.25%
+----------------------------------------
+-- Office Supplies  | 15.6%
+----------------------------------------
+-- Furniture        | 15.9%
+----------------------------------------
+
+
+
+-- Q7) Calculate customer behavior metrics as shown below : 
+-- Total spend by customer
+-- Total time active as a customer
+-- Total orders placed
+-- Average basket Value = Total spent_amount/Total orders
+
+-- Output format : 
+---------------------------------------------------------------------------------------------------------------------------------
+-- CustomerID | Total_Amt_Spent  |  Total_Time_Active  | Total_Orders | Average Basket Value |
+----------------------------------------
+-- DK-1003    | 700.64           |
+----------------------------------------
+-- AA-1004    | 453.22           |
+----------------------------------------
+-- EM-1005    | 1523.76          |
+----------------------------------------
+
+
